383249dd0d3d552a55cb97f6b1d39307afdaeb87
Alexander <alexandr290497@gmail.com>
2017-10-28 14:14:17 +0300

    Get instance and print it

--- src/main/java/com/teaminternational/web/MyHttpServlet.java
+++ src/main/java/com/teaminternational/web/MyHttpServlet.java
@@ -1,4 +1,26 @@
 package com.teaminternational.web;
 
+import com.teaminternational.HelloWorldGenerator;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class MyHttpServlet extends BaseHttpServlet {
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HelloWorldGenerator hwg = getBean(HelloWorldGenerator.COMPONENT_NAME);
+        PrintWriter pw = resp.getWriter();
+        pw.write("<!DOCTYPE html>" +
+                "<html lang=\"en\">" +
+                "<head>" +
+                "<meta charset=\"UTF-8\">" +
+                "<title>HelloWorldServlet</title>" +
+                "</head>" +
+                "<body>");
+        pw.write(hwg.sayHello());
+        pw.write("</body></html>");
+    }
 }
